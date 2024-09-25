758e6c5c268306e0ea582cc15f0fcc5b56bd9766
kauaprass <160037186+kauaprass@users.noreply.github.com>
2024-09-25 19:50:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Título 1',
+    'significado da palavra speed',
+    'velocidade'
+)
+
+criaCartao(
+    'Título 2',
+    'significado da palavra yellow',
+    'amarelo'
+)
+
+criaCartao(
+    'Título 3',
+    'significado da palavra blue',
+    'azul'
+)
+
+criaCartao(
+    'Título 4',
+    'qual o segundo nome do professor',
+    'honorato'
+)
+
+criaCartao(
+    'Título 5',
+    'quem é o camisa 10 do real madrid?',
+    'modric'
+)
+
+criaCartao(
+    'Título 6',
+    'Pergunta 6',
+    'Resposta 6'
+)
+
+criaCartao(
+    'Título 7',
+    'Pergunta 7',
+    'Resposta 7'
+)
+
+criaCartao(
+    'Título 8',
+    'Pergunta 8',
+    'Resposta 8'
+)
+
+criaCartao(
+    'Título 9',
+    'Pergunta 9',
+    'Resposta 9'
+)
+
+criaCartao(
+    'Título 10',
+    'Pergunta 10',
+    'Resposta 10'
+)
